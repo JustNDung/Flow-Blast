b9ff8ffe1d43dccf634addfaa598a9da51299f86
Dungnn <nguyenngocdung19122005@gmail.com>
2026-06-13 00:03:14 +0700

    Implement conveyor belt logic with serialized item spacing, speed, and movement through line renderer

--- Assets/Scripts/ConveyorBelt/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt/ConveyorBelt.cs
@@ -1,16 +1,55 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour
+namespace ConveyorBelt
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ConveyorBelt : MonoBehaviour
     {
-        
-    }
+        [System.Serializable]
+        public class ConveyorBeltItem
+        {
+            public Transform item;
+            [HideInInspector] public float currentLerp;
+            [HideInInspector] public int startPoint = 1;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private float itemSpacing;
+        [SerializeField] private float speed;
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private List<ConveyorBeltItem> items;
+
+        private void FixedUpdate()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                ConveyorBeltItem beltItem = items[i];
+                Transform item = items[i].item;
+
+                if (i > 0)
+                {
+                    if (Vector3.Distance(item.position, items[i - 1].item.position) <= itemSpacing)
+                    {
+                        continue;
+                    }
+                }
+                
+                item.transform.position = Vector3.Lerp(lineRenderer.GetPosition(beltItem.startPoint), lineRenderer.GetPosition(beltItem.startPoint + 1), beltItem.currentLerp);
+                float distance = Vector3.Distance(lineRenderer.GetPosition(beltItem.startPoint), lineRenderer.GetPosition(beltItem.startPoint + 1));
+                beltItem.currentLerp += speed * Time.deltaTime / distance;
+
+                if (beltItem.currentLerp >= 1)
+                {
+                    if (beltItem.startPoint + 2 < lineRenderer.positionCount)
+                    {
+                        beltItem.currentLerp = 0;
+                        beltItem.startPoint++;
+                    }
+                    else
+                    {
+                    
+                    }
+                }
+            }
+        }
     }
 }
